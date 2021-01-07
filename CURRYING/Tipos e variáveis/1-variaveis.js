0bdffdbaf7b6d172eb409cccd942e37a5e2db8e7
leonardongarcia <leonprogramacao@gmail.com>
2021-01-07 16:53:23 -0300

    Tipos e Variáveis

---
+++ CURRYING/Tipos e variáveis/1-variaveis.js
@@ -0,0 +1,19 @@
+var nameVar = 'Leonardo'
+let nameLet = 'Leonardo'
+const nameConst = 'Leonardo'
+
+console.log(`nameVar: ${nameVar}`)
+console.log(`nameLet: ${nameLet}`)
+console.log(`nameConst: ${nameConst}`)
+
+// escopo global
+
+{
+    //escopo de bloco
+
+}
+
+function test() {
+
+    //escopo de função
+}
