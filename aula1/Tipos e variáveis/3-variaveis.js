603c5dc9f0aeaf8628bb0011bef33fa7d459335e
leonardongarcia <leonprogramacao@gmail.com>
2021-01-08 13:05:36 -0300

    Finalização aula 1

---
+++ aula1/Tipos e variáveis/3-variaveis.js
@@ -0,0 +1,29 @@
+const name = 'Leonardo';
+
+//Não podemos alterar o valor
+name = 'Leonardo';
+
+const = user = {
+    name: 'Leonardo'
+};
+
+//Mas se for um objeto, podemos trocar suas propriedades
+user.name = 'Outro nome';
+
+//Não podemos fazer o objeto "apontar" para outro lugar
+user = {
+    name: 'Leonardo'
+};
+
+const persons = ['Leonardo', 'Pedro', 'Jennifer'];
+
+//Podemos adicionar novos itens
+persons.push('João');
+
+//Podemos remover algum item
+persons.shift();
+
+//Podemos alterar diretamente
+persons[1] = 'James';
+
+console.log('\nArray após as alterações: ', persons);
