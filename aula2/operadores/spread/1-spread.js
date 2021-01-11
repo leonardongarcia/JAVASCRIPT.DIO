f398a8e1fd710175ebe1b2723dd27efef5f2bf4d
leonardongarcia <leonprogramacao@gmail.com>
2021-01-11 12:32:07 -0300

    Create 1-spread.js

---
+++ aula2/operadores/spread/1-spread.js
@@ -0,0 +1,12 @@
+// Spread ...
+Var partes = ['ombro', 'joelhos'];
+var musica = ['cabeca', ...partes, 'e', 'pés'];
+
+
+var musica = ['cabeca', 'ombro', 'joelhos', 'e', 'pes'];
+
+
+
+function fn(x, y, z) { }
+var args = [0, 1, 2];
+fn(...args);
