c0a0e29131e59edb59cf1ff38ceac45f41f3ee60
leonardongarcia <leonprogramacao@gmail.com>
2021-01-11 12:44:27 -0300

    Create 1-comparacao.js

---
+++ aula2/operadores/comparacao/1-comparacao.js
@@ -0,0 +1,32 @@
+// igual (==)
+// Retorna verdadeiro caso os operandos sejam iguais. 3 == var1
+"3" == var1
+3 == '3'
+
+// Não igual (!=)
+// Retorna verdadeiro caso os operandos não sejam iguais. var1 != 4
+var2 != "3"
+
+// Estritamente igual (===)
+// Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. Veja tnm Object.is e igualdade
+var2 === "3"
+
+// Estritamente não igual (!==)
+// Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. Veja tnm Object.is e igualdade
+var2 !== "3"
+
+//maior que (>)
+// Retorna vardadeiro caso o operando da esquerda seja maior que o da direita.
+var2 > var1
+
+//maior ou igual a (>=)
+// Retorna vardadeiro caso o operando da esquerda seja maior ou igual o da direita.
+var2 >= var1
+
+//menor que (<)
+// Retorna vardadeiro caso o operando da esquerda seja menor que o da direita.
+var2 < var1
+
+//menor ou igual a (>=)
+// Retorna vardadeiro caso o operando da esquerda seja menor ou igual o da direita.
+var2 <= var1
